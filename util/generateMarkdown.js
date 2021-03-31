30e6052fa152c7ade59dbf68ed7f479f3a095f89
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-30 20:01:09 -0400

    fixed the table of content traversing

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -20,40 +20,29 @@ function licenseBadge(data) {
  return licenseString
 };
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-
-
-// }
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   renderLicenseBadge
-// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `##Title 
-  ${data.Title}
+${data.Title}
+
+## Description
+${data.Description}
+
+## License
+${licenseBadge(data)}
 
 ## Table of Contents
 * [Description](#Description)
 * [Installation](#Installation)
 * [Usage](#Usage)
 * [License](#License)
-* [Contributors](#Contributors}
+* [Contributors](#Contributors)
 * [Test](#Test)
 
-## Description
-${data.Description}
 
 ## Installation Instruction
 ${data.Usage}
-
-## License
-${licenseBadge(data)}
 
 ## Contributors
 ${data.Contributors}
